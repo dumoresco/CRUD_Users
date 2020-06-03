d790dabcc406d114bfb73514cbbaf90a115a28c6
dumoresco <eduardomorescoiost@gmail.com>
2020-06-02 21:47:57 -0300

    Apenas realizei a parte visual do sistema, ainda não está funcionando :)

---
+++ script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $(".register").hide()
+})
+
+$(document).ready(function(){
+    $(".add").click(function(){
+      $(".register").toggle(1000);
+    });
+  });
+
+
